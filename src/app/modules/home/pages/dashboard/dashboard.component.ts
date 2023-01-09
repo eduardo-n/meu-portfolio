2080601ca638016fb38bf9524d7ef8107221de3f
Eduardo Nunes <edununes726@gmail.com>
2023-01-09 15:12:21 -0300

    Botao para fechar e abrir sidenav

--- src/app/modules/home/pages/dashboard/dashboard.component.ts
+++ src/app/modules/home/pages/dashboard/dashboard.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  isClosedSidenav: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sidenavCloser(drawer: any) {
+    this.isClosedSidenav = !this.isClosedSidenav;
+    drawer.toggle();
+  }
 }
