516e7a2f32641816374a32c25991096a94d73ca9
Eduardo Nunes <edununes726@gmail.com>
2023-01-15 20:28:20 -0300

    Removendo padding da modal

--- src/app/modules/home/components/container-profile/container-profile.component.ts
+++ src/app/modules/home/components/container-profile/container-profile.component.ts
@@ -66,7 +66,8 @@ export class ContainerProfileComponent implements OnInit {
       data: {
         title: skill.name,
         text: skill.explanatoryText
-      }
+      },
+      panelClass: 'custom-modal'
     });
   }
 
